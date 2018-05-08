3e5d94960ccec413b839ac0eb6064ed6a7e2f352
Fedor Dobrovidov <dobrovidoff@gmail.com>
2018-05-08 10:39:20 +0300

    fixed an issue with passing list instead of numpy array

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from keras.preprocessing import sequence
 
+def get_feature (data_item):
+    if (data_item.value < 0):
+        print('target class expected to be 0 or 1, but got: ' + str(data_item.value))
+    
+    x_item = 2*[0]
+    x_item[0] = np.array(news_corp_preprocessor.get_words_embeddings(data_item.string_1))
+    x_item[1] = np.array(news_corp_preprocessor.get_words_embeddings(data_item.string_2))
+    x_item = np.array(x_item)
+
+    y_item = 1*[0]
+    y_item[0] = data_item.value
+
+    return (x_item, y_item)
+
 xml_loader = XmlLoader()
 xml_loader.load('../../Downloads/paraphraser/paraphrases.xml')
 result = xml_loader.parse()
@@ -67,39 +81,17 @@ y_test = list()
 MAX_LENGTH = 7
 
 for i in range (0, 10):
-    res = next(result)
-
-    if (res.value < 0):
-        print('target class expected to be 0 or 1, but got: ' + str(res.value))
-
-    x_item = 2*[0]
-    x_item[0] = news_corp_preprocessor.get_words_embeddings(res.string_1)
-    x_item[1] = news_corp_preprocessor.get_words_embeddings(res.string_2)
-
-    y_item = 1*[0]
-    y_item[0] = res.value
-
+    data_item = next(result)
+    x_item, y_item = get_feature(data_item)
     x_train.append(x_item)
     y_train.append(y_item)
 
 for i in range (21, 30):
-    res = next(result)
-
-    x_item = 2*[0]
-    x_item[0] = np.array(news_corp_preprocessor.get_words_embeddings(res.string_1))
-    x_item[1] = np.array(news_corp_preprocessor.get_words_embeddings(res.string_2))
-
-    y_item = 1*[0]
-    y_item[0] = res.value
-
+    data_item = next(result)
+    x_item, y_item = get_feature(data_item)
     x_test.append(x_item)
     y_test.append(y_item)
 
-feature_train = np.array(x_train)
-target_train = np.array(y_train)
-feature_test = np.array(x_test)
-target_test = np.array(y_test)
-
 nn_model = Model()
-nn_model.fit(feature_train, target_train, 1)
-nn_model.predict(feature_test)
+nn_model.fit(x_train, y_train, 1)
+nn_model.predict(x_test)
